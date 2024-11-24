28685bf94f217beab0410ab7dc3c8a5de8498cd6
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-11-24 23:31:41 +0600

    Create Triangle.cpp

---
+++ C++ practice /Triangle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    float triangle,base,height;
+    cout<<"Take base:";
+    cin>>base;
+    cout<<"Take height:";
+    cin>>height;
+    triangle=0.5*base*height;
+    cout<<"Area of triangle is="<<triangle;
+    return 0;
