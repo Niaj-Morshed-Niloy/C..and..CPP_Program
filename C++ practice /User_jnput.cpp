47fbc9e39815a993fe110a131eab59188f60a4c8
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-11-25 22:29:25 +0600

    Create User_jnput.cpp

---
+++ C++ practice /User_jnput.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int x,y;
+    char ch='a';
+    cout<<"Take integer=";
+    cin>>x>>y;
+    cout<<"X is="<<x<<endl<<"Y is="<<y<<endl;
+    cout<<"character is="<<ch;
+    return 0;
+}
