42a5f9b931a3cfc9fb7bb3b614c63fb2fb4a8529
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-11-27 15:05:43 +0600

    Create size_of.cpp

---
+++ C++ practice /size_of.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a;
+    float f;
+    double d;
+    char ch;
+    char name[20];
+    int c=sizeof(d);
+    cout<<c;
+    return 0;
+}
