cadcaeef4ab33477666b859ca0ce053792a9c8c1
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-11-28 23:02:06 +0600

    Create large_btw_2n.cpp

---
+++ C++ practice /large_btw_2n.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n1,n2;
+    cin>>n1>>n2;
+    if(n1>n2)
+    {
+        cout<<"Large:"<<n1;
+    }
+    else
+    {
+        cout<<"large:"<<n2;
+    }
+    return 0;
+}
+
