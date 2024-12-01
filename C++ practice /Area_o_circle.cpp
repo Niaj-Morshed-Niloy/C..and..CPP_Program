59e5e44805cd2dc4f49e55f7d70fb7d405edca46
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-01 12:28:38 +0600

    Create Area_o_circle.cpp

---
+++ C++ practice /Area_o_circle.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+//#define pi 3.14
+using namespace std;
+int main()
+{
+    double circle,r,PI=3.1416;
+    cout<<"Take input r:";
+    cin>>r;
+    circle=2*PI*r;
+    cout<<"Area of circle="<<circle;
+    return 0;
+}
