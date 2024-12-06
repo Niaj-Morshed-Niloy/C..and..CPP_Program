71bbc61c00149ce555c82508f94fd664de15c27e
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2024-12-06 11:27:49 +0600

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -4,9 +4,9 @@ int main()
 {
     char operation;
     double n1,n2;
-   // printf("Enter an operation(+,-,*,/):");
+  
     scanf("%c",&operation);
-   // printf("Enter two operands:");
+   
     scanf("%lf%lf",&n1,&n2);
     switch(operation)
     {
