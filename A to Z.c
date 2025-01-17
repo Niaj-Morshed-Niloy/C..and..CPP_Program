67cab1a6fe163ae2260d4dc39fa22b73022b57e0
Md. Niaj Morshed <161250020+Niaj-Morshed-Niloy@users.noreply.github.com>
2025-01-17 22:09:51 +0600

    Update A to Z.c

--- A to Z.c
+++ A to Z.c
@@ -9,16 +9,4 @@ int main()
     while(ch<='Z');
         return 0;
 }
-/*
-#include<stdio.h>
-int main()
-{
-    char ch='a';
-    while(ch<='z')
-    {
-        printf("%c\n",ch);
-        ch++;
-    }
-    return 0;
-}
-*/
+
